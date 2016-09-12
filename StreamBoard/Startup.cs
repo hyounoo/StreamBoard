adb9a853ff5bf40a9bf8ae2d7271c89cc4cdb57c
drakefang <hyounoosung@gmail.com>
2016-09-12 14:51:40 +0900

    Project Created

---
+++ StreamBoard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StreamBoard.Startup))]
+namespace StreamBoard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
